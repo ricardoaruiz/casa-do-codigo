98f83388406e733d5483c499bb5e1b157140914c
Ricardo Almendro Ruiz <ricardo.almendro.ruiz@gmail.com>
2016-07-24 19:46:24 -0300

    10. Deploy - Primeiro commit

--- project/app.js
+++ project/app.js
@@ -5,8 +5,10 @@ var io = require('socket.io')(http);
 
 app.set('io', io);
 
+var porta = process.env.PORT || 3000
+
 // Inicia o servidor na porta informada para escutar as requisições
 // que serão tratadas pelo express
-http.listen(3000, function() {
-    console.log('Servidor no ar...')
+http.listen(porta, function() {
+    console.log('Servidor no ar na porta ' + porta)
 });
--- project/app/infra/connectionFactory.js
+++ project/app/infra/connectionFactory.js
@@ -13,7 +13,7 @@ var connectMYSQL = function() {
         return mysql.createConnection({
                 user : 'root',
                 password : 'root',
-                database : 'casadocodigo1',
+                database : 'casadocodigo',
                 host : 'localhost',
                 debug : false
             });
@@ -28,6 +28,27 @@ var connectMYSQL = function() {
             debug : false
         });
     }
+
+    if(process.env.NODE_ENV === 'production') {
+        return mysql.createConnection({
+            user : 'b2f81dbf0f6b41',
+            password : '8c85e947',
+            database : 'heroku_b7557f47d4d778a',
+            host : 'us-cdbr-iron-east-04.cleardb.net',
+            debug : false
+        });
+        /*
+        var urlConexao = process.env.CLEARDB_DATABASE_URL;
+        var grupos = urlConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
+        return mysql.createConnection({
+            user : grupos[1],
+            password : grupos[2],
+            host : grupos[3],
+            database : grupos[4],
+            debug : false
+        });
+        */
+    }    
 }
 
 // Retorna a função connectMYSQL para quem precisar
